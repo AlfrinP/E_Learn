5232a2d7589a6ccbcf3614bf25e962e478eae028
AlfrinP <alfrinpoulose@gmail.com>
2024-06-16 19:12:24 +0530

    initial commit

---
+++ screens/auth/otp.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function OTP() {
+  return <Text>OTP</Text>;
+}
---
+++ screens/auth/password.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function Password() {
+  return <Text>Password</Text>;
+}
+
+export default Password;
